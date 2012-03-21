aafcef9edd3f7590ac4cc13948680970387c85bf
Andrew Grimberg <agrimberg@linuxfoundation.org>
2012-03-21 15:25:38 -0700

    Initial spec file and sdist handling
    Make sure the MANIFEST.in lists everything needed for doing a good sdist
    for use when generating a final RPM.
    
    Also, make our setup.py a little more verbose in it's description, this
    allows for it to generate a basic RPM without the .spec file
    
    Our initial .spec file ignores running the test harness at this time.

--- setup.py
+++ setup.py
@@ -1,14 +1,26 @@
 #!/usr/bin/python -tt
 
+import os
 from distutils.core import setup
 
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+VERSION='0.2.0'
+NAME='totpcgi'
+
 setup(
-    version='0.2.0',
+    version=VERSION,
     url='https://github.com/mricon/totp-cgi',
-    name='totpcgi',
+    name=NAME,
     description='A centralized totp solution based on google-authenticator',
     author='Konstantin Ryabitsev',
     author_email='mricon@kernel.org',
-    packages=['totpcgi'],
+    packages=[NAME],
     license='GPLv2+',
+    long_description=read('README.rst'),
 )
