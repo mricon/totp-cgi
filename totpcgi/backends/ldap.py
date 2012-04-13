4ceef59815261f5d6b2edcf7504c2aec6a0e3199
Konstantin Ryabitsev <mricon@kernel.org>
2012-04-13 11:17:12 -0400

    Use absolute_import to resolve import ambigity.
    As the file is called "ldap.py" and tries to import "ldap",
    the wrong one was being imported.

--- totpcgi/backends/ldap.py
+++ totpcgi/backends/ldap.py
@@ -13,6 +13,8 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
 # 02111-1307, USA.
 #
+from __future__ import absolute_import
+
 import logging
 import totpcgi
 import totpcgi.backends
