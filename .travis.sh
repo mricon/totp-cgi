2e2ab04d208af022cb9f4e99caf53e39f8f218cb
Konstantin Ryabitsev <konstantin@linuxfoundation.org>
2018-10-24 11:21:08 -0400

    Show me the log after you're done
    Signed-off-by: Konstantin Ryabitsev <konstantin@linuxfoundation.org>

--- .travis.sh
+++ .travis.sh
@@ -6,9 +6,12 @@ fi
 
 echo "Running File backend tests"
 python test.py $TEST_FLAGS
+cat test.log
 if [ "$?" != "0" ]; then
     exit $?
 fi
+
+rm -f test.log
 
 echo "Running MySQL backend tests"
 mysql -e 'CREATE DATABASE totpcgi;'
@@ -18,6 +21,7 @@ mysql_connect_host='localhost' \
     mysql_connect_user='travis' \
     mysql_connect_password='' \
     python test.py $TEST_FLAGS
+cat test.log
 if [ "$?" != "0" ]; then
     exit $?
 fi
