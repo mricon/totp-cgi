e0f186776833c078766cf76483da1f4ffec2eeb3
Konstantin Ryabitsev <konstantin@linuxfoundation.org>
2018-10-24 11:49:35 -0400

    Try folding again
    Signed-off-by: Konstantin Ryabitsev <konstantin@linuxfoundation.org>

--- .travis.sh
+++ .travis.sh
@@ -6,12 +6,10 @@ fi
 
 echo "Running File backend tests"
 python test.py $TEST_FLAGS
-echo "travis_fold:start:Start File backend test.log"
+E_FILE=$?
+echo -en 'travis_fold:start:File_backend\\r'
 cat test.log
-echo "travis_fold:end:End File backend test.log"
-if [ "$?" != "0" ]; then
-    exit $?
-fi
+echo -en 'travis_fold:end:File_backend\\r'
 
 rm -f test.log
 
@@ -23,21 +21,23 @@ mysql_connect_host='localhost' \
     mysql_connect_user='travis' \
     mysql_connect_password='' \
     python test.py $TEST_FLAGS
-echo "travis_fold:start:Start MySQL backend test.log"
+E_MYSQL=$?
+echo -en 'travis_fold:start:MySQL_backend\\r'
 cat test.log
-echo "travis_fold:end:End MySQL backend test.log"
-if [ "$?" != "0" ]; then
-    exit $?
-fi
+echo -en 'travis_fold:end:MySQL_backend\\r'
+
+rm -f test.log
 
 echo "Running PostgreSQL backend tests"
 psql -U postgres -c 'create database totpcgi;'
 psql -U postgres -d totpcgi -a -f contrib/postgres.sql
 pg_connect_string='postgresql://localhost/totpcgi' \
     python test.py $TEST_FLAGS
-echo "travis_fold:start:Start PostgreSQL backend test.log"
+E_PSQL=$?
+echo -en 'travis_fold:start:PostgreSQL_backend\\r'
 cat test.log
-echo "travis_fold:end:End PostgreSQL backend test.log"
-if [ "$?" != "0" ]; then
-    exit $?
+echo -en 'travis_fold:end:PostgreSQL_backend\\r'
+
+if [ $E_FILE -gt 0 -o $E_MYSQL -gt 0 -o $E_PSQL -gt 0]; then
+    exit 1
 fi
